9a21736939ae0845c9efc6ea6bb07762ebaad134
tovi <zhongtang2005@126.com>
2015-11-19 00:11:36 +0800

    add lesson 1,2

---
+++ 001/main.c
@@ -0,0 +1,24 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    int a = 5;
+    char c = 'a';
+    float f = 5.3;
+    double m = 12.65;
+    double result;
+
+    printf("a+c= %d\n", a+c);
+    printf("a+c= %c\n", a+c);
+    printf("f+m= %f\n", f+m);
+
+    printf("a+m = %f\n", a+m);
+    printf("c+f = %f\n", c+f);
+
+    result = a+c+(f+m);
+    printf("double = %f\n", result);
+
+    return 0;
+}
+
---
+++ 002/main.c
@@ -0,0 +1,27 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    /*换行符'\n'*/
+    printf("How are you?\n");
+    printf("I am fine.\n\n");
+
+    /*横向跳格符'\t'*/
+    printf("How are you?\t");
+    printf("I am fine.\n\n");
+
+    /*退格符'\b'*/
+    printf("  How are you?\n");
+    printf("  \bI am fine.\n\n");
+
+    /*回车符'\r'*/
+    printf("                I am fine.");
+    printf("\rHow are you?\n\n");
+
+    /*综合使用*/
+    printf("note:\n a s\ti\b\bk\rp\n");
+
+    return 0;
+}
+
